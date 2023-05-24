89d9772a3ad2afcb86e7d98be4216a7da322a0f1
labrac <raquelortegalabrac@hotmail.com>
2023-05-24 20:24:40 +0200

    Add main.tsx

---
+++ src/main.tsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+
+ReactDOM.createRoot(document.querySelector(#root) as HTMLElement).render(
+  <React.StrictMode>
+    <Router>
+      <App />
+    </Router>
+  </React.StrictMode>
+);
